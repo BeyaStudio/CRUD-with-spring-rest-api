32ea033eff8cac818c14da76625bea8308aef5bb
Beya <yuricasadei18@gmail.com>
2025-03-25 23:57:51 -0300

    Acessando Data com JPA

---
+++ CRUD-rest-api/src/main/java/CRUD_rest_api/CrudRestApiApplication.java
@@ -0,0 +1,75 @@
+package CRUD_rest_api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import CRUD_rest_api.acessingdatajpa.Customer;
+import CRUD_rest_api.acessingdatajpa.CustomerRepository;
+
+@SpringBootApplication
+public class CrudRestApiApplication {	
+	
+	private static final Logger log = LoggerFactory.getLogger(CrudRestApiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApiApplication.class, args);
+	}
+	
+	
+	
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		
+		return (args) -> {
+			
+			
+			//Saving a new customer
+			
+			repository.save(new Customer("Yuri", "Casadei"));
+			repository.save(new Customer("Ingrid", "Casadei"));
+			repository.save(new Customer("Igor", "Casadei"));
+			repository.save(new Customer("Gustavo", "Ferrari"));
+			repository.save(new Customer("Igor", "Guimaroes"));
+			
+			
+			
+			//fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			
+			log.info("");
+			
+			
+			//fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findeById(1L):");
+			log.info(customer.toString());
+			log.info("-------------------------------");
+			
+			
+		    // fetch customers by last name
+		    log.info("Customer found with findByLastName('Casadei'):");
+		    log.info("--------------------------------------------");
+			
+		    repository.findByLastName("Casadei").forEach(casadei -> {
+		    	log.info(casadei.toString());
+		    });
+
+		    log.info("");
+			
+			
+			
+		};
+		
+	}
+
+}
---
+++ CRUD-rest-api/src/test/java/CRUD_rest_api/CrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUD_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
